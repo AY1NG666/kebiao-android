0964f1c98b54c25d2c17c503114aa4a9fb1801be
AY1NG666 <ayng666@users.noreply.github.com>
2026-05-10 11:22:14 +0800

    v3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ayng.kebiao"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "3.0"
+        versionCode = 31
+        versionName = "3.0.1"
     }
 
     buildTypes {
